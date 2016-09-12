ae858d7cc8daa2e17e74614790cf438b56be73d5
Yan NA (Ryan NA) <408947166@qq.com>
2016-09-12 14:53:54 +0800

    Update class-s3-uploads.php
    Improve modularity, change multisite configuration into wp-config.php

--- inc/class-s3-uploads.php
+++ inc/class-s3-uploads.php
@@ -16,52 +16,36 @@ class S3_Uploads {
 	 */
 	public static function get_instance() {
 
-		//load your mutlisites' information
-		//"id" is the blog id of each website
-		//"key" is the user's Access Key Id
-		//"bucket" is S3 bucket name
-		//"secret" is the user's Secret Access Key
-		//"region" is the corresponding bucket region 
-		$sites =  array(
-	array("id"=>"YOUR SITE BLOG ID (e.g. 1)",
-		"key"=>"YOUR ACCESS KEY ID",
-		"bucket"=>"YOUR BUCKET NAME FOR THIS SITE",
-		"secret"=>"YOUR SECRET ACCESS KEY",
-		"region"=>"THE REGION OF THIS BUCKET"),
-	array("id"=>"YOUR SITE BLOG ID (e.g. 2)",
-		"key"=>"YOUR ACCESS KEY ID",
-		"bucket"=>"YOUR BUCKET NAME FOR THIS SITE",
-		"secret"=>"YOUR SECRET ACCESS KEY",
-		"region"=>"THE REGION OF THIS BUCKET")
-	);
-
-        
-		//load the default end point of s3-uploads which come from your wp-config.php
-		//This will set up the default S3 end point when there is no value set in $sites
 		if ( ! self::$instance ) {
 			self::$instance = new S3_Uploads(
+				//load the default end point
 				S3_UPLOADS_BUCKET,
 				defined( 'S3_UPLOADS_KEY' ) ? S3_UPLOADS_KEY : null,
 				defined( 'S3_UPLOADS_SECRET' ) ? S3_UPLOADS_SECRET : null,
 				defined( 'S3_UPLOADS_BUCKET_URL' ) ? S3_UPLOADS_BUCKET_URL : null,
 				S3_UPLOADS_REGION
 			);
+			
+			
+
+			$i=1; //load the end points of multisites if exist
+			while(defined('S3_UPLOADS_BUCKET_M1')) {
+
+				if (constant(S3_UPLOADS_ID_M.$i)==get_current_blog_id()) {
+				self::$instance = new S3_Uploads(
+				constant(S3_UPLOADS_BUCKET_M.$i),
+				constant(S3_UPLOADS_KEY_M.$i),
+				constant(S3_UPLOADS_SECRET_M.$i),
+				null,
+				constant(S3_UPLOADS_REGION_M.$i)
+				);
+				break;
+			    }
+
+			    $i++;
+			}
 		}
 
-		//replace the default S3 endpoint by the endpoint of the current website
-		for ($i=0;$i<count($sites);$i++){
-			$thesite=$sites[$i];
-			if ($thesite['id']==get_current_blog_id()) {
-			self::$instance = new S3_Uploads(
-			$thesite['bucket'],
-			$thesite['key'],
-			$thesite['secret'],
-			null,
-			$thesite['region']
-			);
-			break;
-		    }
-	    }
 
 		return self::$instance;
 	}
